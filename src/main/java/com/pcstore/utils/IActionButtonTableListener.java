f7721f1e12dd65409a8b289586ef01c5a236b08d
hieud3663 <hieud3663@gmail.com>
2025-04-04 00:20:13 +0700

    update

--- src/main/java/com/pcstore/utils/IActionButtonTableListener.java
+++
@@ -1,7 +0,0 @@
-package com.pcstore.utils;
-
-public interface IActionButtonTableListener {
-    void onViewButtonClick(int row);
-    void onEditButtonClick(int row);
-    void onDeleteButtonClick(int row);
-}
